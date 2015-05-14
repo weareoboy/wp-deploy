efc8cf0be01e5fa21ee34409677c6efd4d6de1bc
Aaron Thomas <a.thomas@mixd.co.uk>
2015-05-14 17:03:27 +0100

    setup file generation for wp-deploy core files

--- lib/capistrano-wp-deploy/cli.rb
+++ lib/capistrano-wp-deploy/cli.rb
@@ -1,8 +1,22 @@
 require "thor"
 
 class WpdCLI < Thor
-  desc "init", "Initialises the WordPress project"
-  def init
-    say "Hello world"
-  end
+    include Thor::Actions
+
+    def self.source_root
+        File.expand_path("../templates",__FILE__)
+    end
+
+    desc "init", "Initialises the WordPress project"
+    def init
+
+        # Runs `cap install` to setup Capistrano base
+        say "wp-deploy: Setting up Capistrano", :green
+        puts `bundle exec cap install`
+        remove_file("Capfile")
+
+        # Create base WordPress/wp-deploy files
+        say "wp-deploy: Creating WordPress files", :green
+        directory "."
+    end
 end
