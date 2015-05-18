61da3e921d7e19db5f9fa794ca5f689200fda0d7
Aaron Thomas <a.thomas@mixd.co.uk>
2015-05-18 11:46:26 +0100

    better and more legible feedback for init

--- lib/capistrano/wp-deploy/cli.rb
+++ lib/capistrano/wp-deploy/cli.rb
@@ -11,8 +11,33 @@ class WpdCLI < Thor
     def init
 
         # Create base WordPress/wp-deploy files
-        say "wp-deploy: Creating WordPress files", :green
+        say "wp-deploy: Setting up a new wp-deploy project", :green
         directory "."
+
+        # Initialise new git repo
+        say "wp-deploy: Checking if we need a new git repo", :green
+        system('git init') unless Dir.exist?('.git')
+
+        # Fetch WP core submodule
+        say "wp-deploy: Cloning latest WordPress core as a submodule", :green
+        system('
+            git submodule add https://github.com/WordPress/WordPress.git wordpress
+            cd wordpress
+            git fetch --tags
+            latestTag=$(git tag -l | sort -n -r -t. -k1,1 -k2,2 -k3,3 -k4,4 | sed -n 1p)
+            git checkout -q $latestTag
+            cd ../
+            git add -A
+            git commit -m "Set up wp-deploy"
+        ')
+
+        say "
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+wp-deploy:
+
+Good to go! Now populate your `database.yml` and `settings.yml` and
+run `bundle exec wpdeploy setup`
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––", :green
 
     end
 end
